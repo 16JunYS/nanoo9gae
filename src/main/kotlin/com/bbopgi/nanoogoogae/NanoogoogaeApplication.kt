88214f39165370b7db446a311d876cf8099def0a
16JunYS <popo3176junys@gmail.com>
2023-06-24 17:35:25 +0900

    Initial commit

---
+++ src/main/kotlin/com/bbopgi/nanoogoogae/NanoogoogaeApplication.kt
@@ -0,0 +1,11 @@
+package com.bbopgi.nanoogoogae
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NanoogoogaeApplication
+
+fun main(args: Array<String>) {
+	runApplication<NanoogoogaeApplication>(*args)
+}
---
+++ src/test/kotlin/com/bbopgi/nanoogoogae/NanoogoogaeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bbopgi.nanoogoogae
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NanoogoogaeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
